da9be04548e9777889ec2a3c157db0c0349094ce
Michael P <102651473+michmich16@users.noreply.github.com>
2024-10-28 14:05:53 +0100

    nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Paths } from "../../router/Paths";
+import { NavLink } from "react-router-dom";
+import s from './Navbar.module.scss'
 
 export const Navbar = () => {
   const navMenu = [
@@ -8,5 +10,11 @@ export const Navbar = () => {
     },
   ];
 
-  return <nav></nav>;
+  return <nav className={s.navStyle}>
+    <ul className={s.menuStyle}>
+      <li><NavLink>Server</NavLink></li>
+      <li><NavLink>Cart</NavLink></li>
+      <li><NavLink>User</NavLink></li>
+    </ul>
+  </nav>;
 };
